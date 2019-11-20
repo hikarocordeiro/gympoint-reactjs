35b5102516bc3297275a3ddf925e6c023e83c395
Hikaro Cordeiro <hikaro.cordeiro@gmail.com>
2019-11-20 16:55:31 -0300

    signin styles

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Content } from './styles';
+
+import logo from '~/assets/logo.png';
 
 export default function SignIn() {
-  return <div />;
+  return (
+    <Container>
+      <Content>
+        <img src={logo} alt="GymPoint" />
+
+        <form>
+          <label>SEU E-MAIL</label>
+          <input type="email" placeholder="exemplo@email.com" />
+          <label>SUA SENHA</label>
+          <input type="password" placeholder="*************" />
+
+          <button type="submit">Entrar no sistema</button>
+        </form>
+      </Content>
+    </Container>
+  );
 }
---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  height: 100%;
+  background: #ee4d64;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 315px;
+  text-align: center;
+  background: #fff;
+  border-radius: 4px;
+  padding: 45px 30px;
+
+  img {
+    width: 153px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    label {
+      font-size: 14px;
+      color: #444444;
+      text-align: left;
+      font-weight: bold;
+      padding-bottom: 5px;
+    }
+
+    input {
+      color: #444444;
+      background: #fff;
+      border: 1px solid #eee;
+      border-radius: 4px;
+      height: 44px;
+      padding: 0 15px;
+      margin: 0 0 10px;
+      font-size: 16px;
+
+      &::placeholder {
+        color: rgba(0, 0, 0, 0.3);
+      }
+    }
+
+    button {
+      margin: 5px 0 0;
+      height: 44px;
+      background: #ee4d64;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#ee4d64')};
+      }
+    }
+  }
+`;
