694efacd024f05c36c92ad96b41af5aac876a346
Hikaro Cordeiro <hikaro.cordeiro@gmail.com>
2019-11-21 14:06:48 -0300

    persist authentication

--- src/pages/Student/index.js
+++ src/pages/Student/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import api from '~/services/api';
 
 // import { Container } from './styles';
 
 export default function Student() {
-  return <div />;
+  api.get('/students');
+  return <h1>Students</h1>;
 }
