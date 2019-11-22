293869cf65739432bf31bc70f5968be193f19c90
Hikaro Cordeiro <hikaro.cordeiro@gmail.com>
2019-11-22 16:50:01 -0300

    styles for students

--- src/pages/Student/index.js
+++ src/pages/Student/index.js
@@ -1,7 +1,76 @@
 import React from 'react';
+import { MdSearch } from 'react-icons/md';
 
-import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Search,
+  StudentTable,
+  EditButton,
+  DeleteButton,
+} from './styles';
+import RegisterButton from '~/components/Buttons/RegisterButton';
 
 export default function Student() {
-  return <Container>Students</Container>;
+  return (
+    <Container>
+      <Header>
+        <strong>Gerenciando alunos</strong>
+        <aside>
+          <RegisterButton onClick={() => {}} />
+          <Search>
+            <MdSearch size={20} color="#999999" />
+            <input placeholder="Buscar aluno" />
+          </Search>
+        </aside>
+      </Header>
+      <StudentTable>
+        <thead>
+          <tr>
+            <th>NOME</th>
+            <th>E-MAIL</th>
+            <th>IDADE</th>
+            <th>&nbsp;</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Cha Ji-Hum</td>
+            <td>examplet@rocketseat.com.br</td>
+            <td>20</td>
+            <td>
+              <div>
+                <EditButton type="button">editar</EditButton>
+                <DeleteButton type="button">apagar</DeleteButton>
+              </div>
+            </td>
+          </tr>
+
+          <tr>
+            <td>Cha Ji-Hum</td>
+            <td>examplet@rocketseat.com.br</td>
+            <td>20</td>
+            <td>
+              <div>
+                <EditButton type="button">editar</EditButton>
+                <DeleteButton type="button">apagar</DeleteButton>
+              </div>
+            </td>
+          </tr>
+
+          <tr>
+            <td>Cha Ji-Hum</td>
+            <td>examplet@rocketseat.com.br</td>
+            <td>20</td>
+            <td>
+              <div>
+                <EditButton type="button">editar</EditButton>
+                <DeleteButton type="button">apagar</DeleteButton>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </StudentTable>
+    </Container>
+  );
 }
--- src/pages/Student/styles.js
+++ src/pages/Student/styles.js
@@ -1,3 +1,116 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+export const Header = styled.div`
+  height: 86px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  strong {
+    font-weight: bold;
+    font-size: 24px;
+  }
+
+  aside {
+    display: flex;
+    align-self: center;
+    align-items: center;
+  }
+`;
+
+export const Search = styled.div`
+  display: block;
+  max-width: 237px;
+  height: 36px;
+  margin-left: 16px;
+
+  input {
+    width: 237px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    font-size: 14px;
+    line-height: 16px;
+    color: #999;
+    padding: 10px 40px;
+  }
+
+  svg {
+    position: absolute;
+    margin: 12px 15px;
+    line-height: 23px;
+  }
+`;
+
+export const StudentTable = styled.table`
+  width: 100%;
+  background: #fff;
+  border-radius: 4px;
+  padding: 30px 30px;
+  border-spacing: 0px;
+
+  thead {
+    color: #444444;
+    font-size: 16px;
+    font-weight: bold;
+
+    th {
+      text-align: left;
+      padding-bottom: 10px;
+    }
+
+    th:nth-of-type(3) {
+      text-align: center;
+    }
+  }
+
+  tbody {
+    color: #666666;
+    & + tr {
+      border-top: 1px solid #eee;
+    }
+
+    tr {
+      td {
+        font-size: 16px;
+        padding: 20px 0;
+        color: #666666;
+        border-bottom: 1px solid #eee;
+      }
+
+      td:nth-of-type(3) {
+        text-align: center;
+      }
+
+      & + tr:last-child {
+        td {
+          border: none;
+        }
+      }
+    }
+
+    div {
+      display: flex;
+      justify-content: flex-end;
+
+      button {
+        border: 0;
+        background: inherit;
+      }
+    }
+  }
+`;
+
+export const EditButton = styled.button`
+  color: #4d85ee;
+  margin-right: 23px;
+`;
+
+export const DeleteButton = styled.button`
+  color: #de3b3b;
+`;
