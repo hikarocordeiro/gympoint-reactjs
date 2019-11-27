22d6ee5cfc851698ee2248504e043f109895ed5d
Hikaro Cordeiro <hikaro.cordeiro@gmail.com>
2019-11-27 15:54:53 -0300

    enrollment create

--- src/pages/Enrollment/EnrollmentForm/index.js
+++ src/pages/Enrollment/EnrollmentForm/index.js
@@ -1,128 +1,144 @@
-import React, { useEffect, useState, useMemo } from 'react';
-import { useParams } from 'react-router-dom';
-import { Form, Input } from '@rocketseat/unform';
+import React, { useState, useEffect, useMemo } from 'react';
 import { toast } from 'react-toastify';
-import AsyncSelect from 'react-select/lib/Async';
-import Select from 'react-select';
+import {
+  format,
+  addMonths,
+  setHours,
+  setMinutes,
+  setSeconds,
+  endOfSecond,
+} from 'date-fns';
+
+import pt from 'date-fns/locale/pt-BR';
+
+import { Input, Form } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
-import history from '~/services/history';
-import api from '~/services/api';
 import { formatPrice } from '~/util/format';
+import api from '~/services/api';
+import history from '~/services/history';
+
+import ContentHeader from '~/components/ContentHeader';
+import InputAsyncSelect from '~/components/InputAsyncSelect';
+import DatePicker from '~/components/InputDatePicker';
+import ReactSelect from '~/components/InputSelect';
 
 import { Container, Content, InLine } from './styles';
-import ContentHeader from '~/components/ContentHeader';
 
 const schema = Yup.object().shape({
-  student: Yup.string().required('Informe o aluno'),
-  plan: Yup.string().required('Informe o plano'),
-  start_date: Yup.string().required('Informe a data de início'),
+  student: Yup.object()
+    .shape({
+      value: Yup.number().integer(),
+    })
+    .typeError('Valor inválido')
+    .required('Aluno obrigatório'),
+  plan: Yup.object()
+    .shape({
+      value: Yup.number().integer(),
+    })
+    .typeError('Valor inválido')
+    .required('Aluno obrigatório'),
+  start_date: Yup.date()
+    .typeError('Valor inválido')
+    .required('Data obrigatória'),
 });
 
-export default function EnrollmentForm() {
-  const { id } = useParams();
-  const [enrollment, setEnrollments] = useState({});
-  // const [studentList, setStudentList] = useState({});
+export default function RegisterEnrollment() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [plans, setPlans] = useState({});
+  const [plan, setPlan] = useState({});
+  const [initialData, setInitialData] = useState({});
 
-  async function loadEnrollment(enrollmentId) {
-    const response = await api.get(`/enrollments/${enrollmentId}`, {
-      params: {
-        id: enrollmentId,
-      },
-    });
+  async function loadPlans() {
+    const response = await api
+      .get('plans')
+      .then(r => r.data)
+      .then(d =>
+        d.map(p => ({
+          label: p.title,
+          value: p.id,
+          duration: p.duration,
+          price: p.price,
+        }))
+      );
 
-    setEnrollments(response.data);
+    setPlans(response);
   }
 
-  const totalPrice = useMemo(() => {
-    if (enrollment.duration && enrollment.price) {
-      return formatPrice(enrollment.duration * enrollment.price);
+  const end_date = useMemo(() => {
+    if (!plan.duration) {
+      return '';
     }
-    return formatPrice(0);
-  }, [enrollment.duration, enrollment.price]);
+    const { duration } = plan;
+    const formattedDate = format(
+      addMonths(startDate, duration),
+      "dd'/'MM'/'yyyy",
+      {
+        locale: pt,
+      }
+    );
+    return formattedDate;
+  }, [plan, startDate]);
+
+  const totalPrice = useMemo(() => {
+    if (!plan.price) return '';
+
+    return formatPrice(Number(plan.duration) * Number(plan.price));
+  }, [plan.duration, plan.price]);
 
   useEffect(() => {
-    if (id) {
-      loadEnrollment(id);
+    loadPlans();
+
+    setInitialData({
+      end_date,
+      totalPrice,
+    });
+  }, [end_date, startDate, totalPrice]);
+
+  async function handleSubmit(data) {
+    try {
+      const dateNow = new Date();
+      const startDateNow = endOfSecond(
+        setSeconds(
+          setMinutes(
+            setHours(data.start_date, dateNow.getHours()),
+            dateNow.getMinutes()
+          ),
+          dateNow.getSeconds()
+        )
+      );
+      await api.post('enrollments', {
+        student_id: data.student.value,
+        plan_id: data.plan.value,
+        start_date: startDateNow,
+      });
+      toast.success('Cadastro realizado com sucesso');
+      history.push('/enrollment');
+    } catch (e) {
+      toast.error('erro no cadastro');
     }
-  }, [id]);
+  }
+
+  async function loadOptions(inputValue) {
+    const response = await api
+      .get('students', { params: { name: `${inputValue}` } })
+      .then(r => r.data)
+      .then(r =>
+        r.map(student => ({
+          label: student.name,
+          value: student.id,
+        }))
+      );
+    return response;
+  }
 
   function handleBackPage() {
     history.push('/enrollment');
   }
 
-  async function handleSubmit({ student, plan, start_date }) {
-    try {
-      if (!id) {
-        await api.post('/enrollments', { student, plan, start_date });
-
-        toast.success('Cadastro realizado com sucesso');
-      } else {
-        await api.put(`/enrollments/${id}`, {
-          id,
-          student,
-          plan,
-          start_date,
-        });
-
-        toast.success('Cadastro alterado com sucesso');
-      }
-
-      history.push('/enrollment');
-    } catch (err) {
-      toast.error('Erro no cadastro');
-    }
-  }
-
-  async function filterStudents(inputValue) {
-    const value = inputValue.replace(/\W/g, '');
-
-    const response = await api.get('/students', {
-      params: {
-        name: value,
-      },
-    });
-
-    const options = response.data.map(student => {
-      const option = {};
-
-      option.value = student.id;
-      option.label = student.name;
-
-      return option;
-    });
-
-    return options;
-  }
-
-  const promiseOptions = inputValue =>
-    new Promise(resolve => {
-      setTimeout(() => {
-        resolve(filterStudents(inputValue));
-      }, 1000);
-    });
-
-  const customStyles = {
-    option: (provided, state) => ({
-      ...provided,
-      color: state.isSelected ? '#999999' : '#999999',
-    }),
-    control: () => ({
-      border: '1px solid #dddddd',
-      borderRadius: '4px',
-      display: 'flex',
-      width: '100%',
-      height: '45px',
-    }),
-    singleValue: provided => ({
-      ...provided,
-      color: '#999999',
-    }),
-  };
-
   return (
     <Container>
-      <Form schema={schema} onSubmit={handleSubmit} initialData={enrollment}>
+      <Form schema={schema} onSubmit={handleSubmit} initialData={initialData}>
         <ContentHeader
           title="Cadastro de matrícula"
           onClickBack={handleBackPage}
@@ -131,33 +147,37 @@ export default function EnrollmentForm() {
         <Content>
           <Input type="hidden" name="id" />
           <strong>ALUNO</strong>
-          <AsyncSelect
-            cacheOptions
-            defaultOptions
-            loadOptions={promiseOptions}
-            placeholder="Selecione o aluno..."
-            styles={customStyles}
-          />
+          <InputAsyncSelect name="student" loadOptions={loadOptions} />
           <br />
           <InLine>
             <div>
               <strong>PLANO</strong>
-              <Select name="plan" type="number" styles={customStyles} />
+              <ReactSelect name="plan" options={plans} setChange={setPlan} />
             </div>
 
             <div>
               <strong>DATA DE INÍCIO</strong>
-              <Input name="start_date" />
+              <DatePicker name="start_date" setChange={setStartDate} />
             </div>
 
             <div>
               <strong>DATA DE TÉRMINO</strong>
-              <Input name="end_date" readOnly />
+              <Input
+                type="data"
+                name="end_date"
+                readOnly
+                className="readOnly"
+              />
             </div>
 
             <div>
               <strong>PREÇO TOTAL</strong>
-              <Input name="totalPrice" readOnly value={totalPrice} />
+              <Input
+                type="text"
+                name="totalPrice"
+                readOnly
+                className="readOnly"
+              />
             </div>
           </InLine>
         </Content>
